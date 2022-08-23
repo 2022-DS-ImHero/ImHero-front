85c38816adc6e845eeb4d7864a987881671baec3
harim061 <doris5093@duksung.ac.kr>
2022-08-24 04:20:56 +0900

    공주 잔다..

--- js/main.js
+++ js/main.js
@@ -54,9 +54,6 @@ function hidenav(){
 }
 
 
-$(document).ready(function(){
-    // 실행할 기능을 정의해주세요.
-    $("#datepicker").datepicker({
-    dateFormat: 'yy-mm-dd'
-    });
+$("#datepicker").datepicker( {
+    format: "mm-yyyy",
 });
